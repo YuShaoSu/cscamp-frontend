fd57a5ee10248666dd5f755ec7f139514b3854dd
Chih-Yu <lcd010308@gmail.com>
2019-08-07 16:17:26 +0800

    Refactor some style

--- src/components/Video/index.js
+++ src/components/Video/index.js
@@ -3,7 +3,6 @@ import MyVid from 'assets/videos/intro.mp4'
 import styles from './style.module.css'
 
 const Video = () => {
-	console.log(styles)
   return (
     <video className={styles.backgroundVideo} loop autoPlay muted width='100%'>
       <source src={MyVid} type='video/mp4' />
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,13 +6,9 @@ import styles from './style.module.css'
 
 const Home = () => {
   return (
-    <div className='container'>
+    <div className='container-fluid bg-black'>
       <Video />
       <img className={styles.posterHeader} src={Poster} alt='header' />
-      <svg width='100%' height='100%' className='sline-container'>
-        {/* <path d="M275,200 v-150" className="sline" /> */}
-        <line x1='350' y1='100' x2='350' y2='155' className='sline' />
-      </svg>
       <p className={styles.introPara}>
         那年夏天，與你一起交資的夢 <br />
         在這裡紀念， <br />

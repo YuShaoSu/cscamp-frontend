8ded93550e345032588f19304aad60a7fd17ef13
Chih-Yu <lcd010308@gmail.com>
2019-08-08 21:08:57 +0800

    Modify navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -3,19 +3,19 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { logout } from 'api/Actions/User'
-import styles from './style.module.scss'
 
 class Navbar extends React.Component {
   render () {
     return (
       <nav className='navbar navbar-expand-md navbar-light bg-light border-bottom px-5'>
-        <div className='navbar-brand'>你不資到的事</div>
+        <div className='navbar-brand mx-3'>你不資道的事</div>
         <div className='collapse navbar-collapse'>
-          <div className='navbar-nav ml-auto px-5'>
-            <Link className='nav-item nav-link' to='/memory'>回憶</Link>
-            <Link className='nav-item nav-link mx-5' to='/performance'>表演</Link>
+          <div className='navbar-nav ml-auto'>
+            <Link className='nav-item nav-link mx-4' to='/look_back'>回憶</Link>
+            <Link className='nav-item nav-link mx-4' to='/performance'>表演</Link>
+            <Link className='nav-item nav-link mx-4' to='/course'>課程</Link>
             <div
-              className='nav-item nav-link clickable'
+              className='nav-item nav-link clickable mx-5'
               onClick={() => this.props.logout()}
             >
               登出

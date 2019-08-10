3dcab271a45891c07bdeda1058034104a5900f36
Chih-Yu <lcd010308@gmail.com>
2019-08-10 17:45:56 +0800

    Add layout

---
+++ src/components/Layout/index.js
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import Navbar from 'components/Navbar'
+
+const Layout = (props) => (
+  <div>
+    <Navbar />
+    <div className='container mt-5'>
+      { props.children }
+    </div>
+  </div>
+)
+
+export default Layout
--- src/components/VideoLayout/index.js
+++ src/components/VideoLayout/index.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import Video from 'components/Video'
 
-const VideoLayout = (props) => {
-  return (
-    <div className='container-fluid bg-black'>
-      <Video />
-      { props.children }
-    </div>
-  )
-}
+const VideoLayout = (props) => (
+  <div className='container-fluid bg-black'>
+    <Video />
+    { props.children }
+  </div>
+)
 
 export default VideoLayout

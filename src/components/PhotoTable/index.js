5e30a298eec99b364395bfb08ef6af350cb3c542
Chih-Yu <lcd010308@gmail.com>
2019-08-12 17:14:10 +0800

    Feat/responsive (#6)
    * Add navbar responsive style
    
    * Add login page responsive style
    
    * Add lookback page responsive style

--- src/components/PhotoTable/index.js
+++ src/components/PhotoTable/index.js
@@ -5,16 +5,16 @@ import styles from './style.module.scss'
 const PhotoLeft = (props) => (
   <React.Fragment>
     <div className={`col-6 border-right border-dark ${styles.vcenter}`}>
-      <div className='pull-right mx-5 py-3'>
+      <div className='pull-right mx-3 mx-lg-4 mx-xl-5 py-3'>
         <img
           width='100%'
-          src={'http://' + props.image}
+          src={props.image}
           alt='無法載入圖片'
         />
       </div>
     </div>
     <div className={`col-6 ${styles.vcenter}`}>
-      <div className='pull-left mx-5 py-3'>
+      <div className='pull-left mx-3 mx-lg-4 mx-xl-5 py-3'>
         <div>{ props.text }</div>
       </div>
     </div>
@@ -24,17 +24,37 @@ const PhotoLeft = (props) => (
 const PhotoRight = (props) => (
   <React.Fragment>
     <div className={`col-6 border-right border-dark ${styles.vcenter}`}>
-      <div className='pull-right mx-5 py-3'>
+      <div className='pull-right mx-3 mx-lg-4 mx-xl-5 py-3'>
         <div>{ props.text }</div>
       </div>
     </div>
     <div className={`col-6 ${styles.vcenter}`}>
-      <div className='pull-left mx-5 py-3'>
+      <div className='pull-left mx-3 mx-lg-4 mx-xl-5 py-3'>
         <img
           width='100%'
-          src={'http://' + props.image}
+          src={props.image}
           alt='無法載入圖片'
         />
+      </div>
+    </div>
+  </React.Fragment>
+)
+
+// for small screen size
+const PhotoUp = (props) => (
+  <React.Fragment>
+    <div className='col-11 col-sm-10 mt-3'>
+      <div className='mx-3 mx-lg-4 mx-xl-5 py-3'>
+        <img
+          width='100%'
+          src={props.image}
+          alt='無法載入圖片'
+        />
+      </div>
+    </div>
+    <div className='col-11 col-sm-10 mb-3'>
+      <div className='mx-3 mx-lg-4 mx-xl-5 py-sm-3'>
+        <div className='px-1'>{ props.text }</div>
       </div>
     </div>
   </React.Fragment>
@@ -45,14 +65,14 @@ const PhotoTable = (props) => {
 
   return (
     <div>
-      <div className='row my-5'>
-        <div className='col-12'>
-          <div className={`${styles.day}`}>
-            { days[props.day] }
+      <div className='row justify-content-center my-4 my-lg-5'>
+        <div className='col-11 col-sm-10'>
+          <div className={`${styles.day} mx-3`}>
+            <span>{ days[props.day] }</span>
           </div>
         </div>
       </div>
-      <div className={`row row-eq-height my-3 ${styles.description}`}>
+      <div className={`row row-eq-height d-none d-md-flex my-3 ${styles.description}`}>
         {
           props.photos.map((photo, index) => {
             return (index % 2 === 0)
@@ -67,6 +87,17 @@ const PhotoTable = (props) => {
                 text={photo.text}
               />
           })
+        }
+      </div>
+      <div className={`row justify-content-center d-md-none my-3 ${styles.description}`}>
+        {
+          props.photos.map((photo, index) => (
+            <PhotoUp
+              key={index}
+              image={photo.image}
+              text={photo.text}
+            />
+          ))
         }
       </div>
     </div>

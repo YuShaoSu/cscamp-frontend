4512b580f7e60eb64805806e5b0239343ed4a522
Chih-Yu <lcd010308@gmail.com>
2019-08-13 15:47:53 +0800

    Add all performance video

--- src/pages/Performance/CampFire/index.js
+++ src/pages/Performance/CampFire/index.js
@@ -5,21 +5,62 @@ import YoutubeVideo from 'components/YoutubeVideo'
 
 class CampFire extends React.Component {
   render () {
+    const videos = [
+      {
+        title: '拜火(視角1)',
+        url: 'https://www.youtube.com/embed/A0Xh6eBuvhE'
+      },
+      {
+        title: '拜火(視角2)',
+        url: 'https://www.youtube.com/embed/bBoudapgqD8'
+      },
+      {
+        title: '螢光(視角1)',
+        url: 'https://www.youtube.com/embed/Q45-OuQWNZ0'
+      },
+      {
+        title: '螢光(視角2)',
+        url: 'https://www.youtube.com/embed/DA95yxl0mQw'
+      },
+      {
+        title: '蛇舞(視角1)',
+        url: 'https://www.youtube.com/embed/0JhP3Cc6LYw'
+      },
+      {
+        title: '蛇舞(視角2)',
+        url: 'https://www.youtube.com/embed/LM6_lWRnKXU'
+      },
+      {
+        title: '光棍(視角1)',
+        url: 'https://www.youtube.com/embed/xRKCKEPrweE'
+      },
+      {
+        title: '光棍(視角2)',
+        url: 'https://www.youtube.com/embed/gwM6PSjCp7E'
+      },
+      {
+        title: 'poi(視角1)',
+        url: 'https://www.youtube.com/embed/gi6YUONEliw'
+      },
+      {
+        title: 'poi(視角2)',
+        url: 'https://www.youtube.com/embed/Jld99W8HbO0'
+      }
+    ]
+
     return (
       <Layout>
         <div className='row justify-content-center'>
-          <div className='col-12 col-md-10 col-lg-8 my-4 my-md-5'>
-            <YoutubeVideo
-              title='蛇舞(視角二)'
-              src='https://www.youtube.com/embed/xb5C10nWBYo'
-            />
-          </div>
-          <div className='col-12 col-md-10 col-lg-8 my-4 my-md-5'>
-            <YoutubeVideo
-              title='光球(視角一)'
-              src='https://www.youtube.com/embed/4N440EYcweQ'
-            />
-          </div>
+          {
+            videos.map((video, index) => (
+              <div className='col-12 col-md-10 col-lg-8 my-4 my-md-5' key={video.title}>
+                <YoutubeVideo
+                  title={video.title}
+                  src={video.url}
+                />
+              </div>
+            ))
+          }
         </div>
       </Layout>
     )
--- src/pages/Performance/NightShow/index.js
+++ src/pages/Performance/NightShow/index.js
@@ -5,21 +5,50 @@ import YoutubeVideo from 'components/YoutubeVideo'
 
 class NightShow extends React.Component {
   render () {
+    const videos = [
+      {
+        title: 'locking',
+        url: 'https://www.youtube.com/embed/fykJn-Mh_vI'
+      },
+      {
+        title: '我的瘦身 失敗',
+        url: 'https://www.youtube.com/embed/vDOiMtfO-NY'
+      },
+      {
+        title: '當個嘻哈囝',
+        url: 'https://www.youtube.com/embed/54sNVLa38cI'
+      },
+      {
+        title: '你媽一番鮮',
+        url: 'https://www.youtube.com/embed/AOujKaqMziw'
+      },
+      {
+        title: '笑話一籮筐',
+        url: 'https://www.youtube.com/embed/A4qqgjN6siw'
+      },
+      {
+        title: '凱文凱勒',
+        url: 'https://www.youtube.com/embed/LC3oOaTvh4Y'
+      },
+      {
+        title: '台灣水電工',
+        url: 'https://www.youtube.com/embed/6IG3oTBPNho'
+      }
+    ]
+
     return (
       <Layout>
         <div className='row justify-content-center'>
-          <div className='col-12 col-md-10 col-lg-8 my-4 my-md-5'>
-            <YoutubeVideo
-              title='女舞'
-              src='https://www.youtube.com/embed/7MNH9CAlzx8'
-            />
-          </div>
-          <div className='col-12 col-md-10 col-lg-8 my-4 my-md-5'>
-            <YoutubeVideo
-              title='Popping'
-              src='https://www.youtube.com/embed/jhmV1M4XCzs'
-            />
-          </div>
+          {
+            videos.map((video, index) => (
+              <div className='col-12 col-md-10 col-lg-8 my-4 my-md-5' key={video.title}>
+                <YoutubeVideo
+                  title={video.title}
+                  src={video.url}
+                />
+              </div>
+            ))
+          }
         </div>
       </Layout>
     )

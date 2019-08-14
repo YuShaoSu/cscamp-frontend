bbcade9734d98af77c738339723e8b63b80a9da1
YuShaoSu <n270882@gmail.com>
2019-08-14 14:25:16 +0800

    change navbar to sidebar (#9)
    add responsive sidebar
    change font size
    change img size

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,7 +1,8 @@
 
 import React from 'react'
 import { connect } from 'react-redux'
-import { Link } from 'react-router-dom'
+import { withRouter, Link, NavLink } from 'react-router-dom'
+import { HashLink } from 'react-router-hash-link'
 import { logout } from 'api/Actions/User'
 import styles from './style.module.scss'
 
@@ -50,31 +51,60 @@ class Navbar extends React.Component {
     const { currentUser } = this.props
 
     return (
-      <nav className={`navbar navbar-expand-sm navbar-light px-4 px-md-5 py-0 ${styles.navbar}`}>
-        <div className={`navbar-brand mx-0 mx-md-3 py-0 ${styles.navbarBrand}`}>你不資道的事</div>
-        <button className='navbar-toggler px-2 py-1' onClick={this.handleClick}>
-          <span className='navbar-toggler-icon' />
-        </button>
-        <div className={`collapse navbar-collapse ${this.state.open && styles.navbarCollapse}`}>
-          <div className={`navbar-nav ml-auto ${styles.navbarNav}`}>
-            <Link className='nav-item nav-link mx-sm-2 mx-md-4 py-0' to='/look_back'>回憶</Link>
-            <NavDropdown title='表演'>
-              <NavDropdownLink to='/performance/night_show'>晚會</NavDropdownLink>
-              <NavDropdownLink to='/performance/camp_fire'>營火</NavDropdownLink>
-            </NavDropdown>
-            <Link className='nav-item nav-link mx-sm-2 mx-md-4 py-0' to='/course'>課程</Link>
-
-            <NavDropdown className='mx-sm-3 mx-md-5' title={currentUser && currentUser.name}>
-              <div
-                className={`dropdown-item clickable ${styles.dropdownItem}`}
-                onClick={() => this.props.logout()}
-              >
-                登出
-              </div>
-            </NavDropdown>
-          </div>
+      <div>
+        <div className={`${styles.sidebar}`}>
+          <button className='navbar-toggler px-2 py-1' onClick={this.handleClick}>
+            <span className='navbar-toggler-icon' />
+          </button>
+            <NavLink className={`mx-sm-4 ${styles.sidebarItem}`} 
+              activeClassName={`${styles.sidebarItemActive}`} to='/look_back'>Look Back</NavLink>
+            <NavLink className={`mx-sm-4 ${styles.sidebarItem}`} 
+              activeClassName={`${styles.sidebarItemActive}`} to='/performance/night_show'>Night Show</NavLink>
+            <NavLink className={`mx-sm-4 ${styles.sidebarItem}`} 
+              activeClassName={`${styles.sidebarItemActive}`} to='/performance/camp_fire'>Camp Fire</NavLink>
+            <NavLink className={`mx-sm-4 ${styles.sidebarItem}`} 
+              activeClassName={`${styles.sidebarItemActive}`} to='/course'>Course</NavLink>
+          {/* <NavDropdown className='mx-sm-3 mx-md-5' title={currentUser && currentUser.name}>
+            <div
+              className={`dropdown-item clickable ${styles.dropdownItem}`}
+              onClick={() => this.props.logout()}
+            >
+              登出
+            </div>
+          </NavDropdown> */}
+          <HashLink smooth to="#Day1" className={`mx-sm-4 ${styles.sidebarHash}`}>Day1</HashLink>
+          <HashLink smooth to="#Day2" className={`mx-sm-4 ${styles.sidebarHash}`}>Day2</HashLink>
+          <HashLink smooth to="#Day3" className={`mx-sm-4 ${styles.sidebarHash}`}>Day3</HashLink>
+          <HashLink smooth to="#Day4" className={`mx-sm-4 ${styles.sidebarHash}`}>Day4</HashLink>
+          <HashLink smooth to="#Day5" className={`mx-sm-4 ${styles.sidebarHash}`}>Day5</HashLink>
+          <HashLink smooth to="#Day6" className={`mx-sm-4 ${styles.sidebarHash}`}>Day6</HashLink>
         </div>
-      </nav>
+
+        <nav className={`navbar navbar-expand-sm navbar-light px-4 px-md-5 py-0 ${styles.navbar}`}>
+          <button className='navbar-toggler px-2 py-1' onClick={this.handleClick}>
+            <span className='navbar-toggler-icon' />
+          </button>
+          <div className={`collapse navbar-collapse ${this.state.open && styles.navbarCollapse}`}>
+            <div className={`navbar-nav ml-auto ${styles.navbarNav}`}>
+              <Link className='nav-item nav-link mx-sm-2 mx-md-4 py-0' to='/look_back'>回憶</Link>
+              <NavDropdown title='表演'>
+                <NavDropdownLink to='/performance/night_show'>晚會</NavDropdownLink>
+                <NavDropdownLink to='/performance/camp_fire'>營火</NavDropdownLink>
+              </NavDropdown>
+              <Link className='nav-item nav-link mx-sm-2 mx-md-4 py-0' to='/course'>課程</Link>
+
+              <NavDropdown className='mx-sm-3 mx-md-5' title={currentUser && currentUser.name}>
+                <div
+                  className={`dropdown-item clickable ${styles.dropdownItem}`}
+                  onClick={() => this.props.logout()}
+                >
+                  登出
+                </div>
+              </NavDropdown>
+            </div>
+          </div>
+        </nav>
+      </div>
     )
   }
 }
@@ -86,4 +116,4 @@ const mapDispatchToProps = (dispatch) => ({
   logout: () => dispatch(logout())
 })
 
-export default connect(mapStateToProps, mapDispatchToProps)(Navbar)
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(Navbar))

f7df0c4dc196b50e4df10aa3b0eb8b2cda7b0d56
Chih-Yu <lcd010308@gmail.com>
2019-08-15 23:37:30 +0800

    Fix/course (#14)
    * Add course api
    
    * Fix some style

---
+++ src/api/Reducers/Course.js
@@ -0,0 +1,15 @@
+
+import { handleActions } from 'redux-actions'
+import { FETCHING_STATUS } from 'utilities/constants'
+
+const initialState = {
+  data: null,
+  status: FETCHING_STATUS.IDLE
+}
+
+export default handleActions({
+  COURSE: {
+    STORE: (state, action) => ({ ...state, data: action.payload }),
+    SET_STATUS: (state, action) => ({ ...state, status: action.payload })
+  }
+}, initialState)
--- src/components/Course/index.js
+++ src/components/Course/index.js
@@ -7,7 +7,7 @@ const CourseCard = (props) => (
     <div className={`col-11 col-sm-10 col-md-6 col-lg-5 ${styles.vcenter}`}>
       <div className='mx-3 mx-lg-4 my-md-3 py-3'>
         <div className={styles.title}>{ props.title }</div>
-        <div className={styles.content}>{ props.content }</div>
+        <div className={styles.text}>{ props.text }</div>
       </div>
     </div>
     <div className={`col-11 col-sm-10 col-md-6 col-lg-5 ${styles.vcenter}`}>
@@ -20,13 +20,13 @@ const CourseCard = (props) => (
 
 const CourseText = (props) => (
   <div className='col-11 col-sm-10 col-md-12 col-lg-10 my-5'>
-    <div className='mx-3 mx-lg-4'>
-      <div className={styles.content}>{ props.content }</div>
+    <div className='mx-3 mx-lg-4 text-center'>
+      <div className={styles.text}>{ props.text }</div>
       <a
         className={styles.link}
         target='_blank'
         rel='noopener noreferrer'
-        href='https://reurl.cc/jLXyp'
+        href='https://drive.google.com/open?id=1RZngBADXut6sUQD0EdIBWRPzwkcCUkWa'
       >
         課程投影片
       </a>

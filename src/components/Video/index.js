eccb78351ae6edb7a36536bb8f3333c1264f82ec
YuShaoSu <n270882@gmail.com>
2019-08-16 00:45:56 +0800

    new intro vid and fix rwd

--- src/components/Video/index.js
+++ src/components/Video/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import MyVid from 'assets/videos/intro.mp4'
+import MyVid from 'assets/videos/intro_background.mp4'
 import styles from './style.module.scss'
 
 const Video = () => {
   return (
-    <video className={styles.backgroundVideo} loop autoPlay muted width='100%'>
+    <video className={styles.backgroundVideo} loop autoPlay muted>
       <source src={MyVid} type='video/mp4' />
     </video>
   )
